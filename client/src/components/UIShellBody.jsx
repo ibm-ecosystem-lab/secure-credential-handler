08c16feb7c255efb0d2c6ecfd0f967162bf61e49
Balasubramanian Srinivasan <bala@Balasubramanians-MacBook-Pro.local>
2020-10-07 08:24:53 +0530

    first commit

---
+++ client/src/components/UIShellBody.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import DisplayForm from "../pattern-components/DisplayForm";
+import ValidatingForm from "../pattern-components/ValidatingForm";
+import UpdateForm from "../pattern-components/UpdateForm";
+import ValidatingFormWizard1 from "../pattern-components/ValidatingFormWizard1";
+import ValidatingFormWizard2 from "../pattern-components/ValidatingFormWizard2";
+import SimpleList from "../pattern-components/SimpleList";
+import TableList from "../pattern-components/TableList";
+import ListToList from "../pattern-components/ListToList";
+import LinkedList from "../pattern-components/LinkedList";
+import MasterDetail from "../pattern-components/MasterDetail";
+import CreateReadUpdateDelete from "../pattern-components/CreateReadUpdateDelete";
+import SearchList from "../pattern-components/SearchList";
+import SearchForm from "../pattern-components/SearchForm";
+import "../pattern-components/patterns.scss";
+
+class UIShellBody extends Component {
+  components = {
+    "Secrets": SimpleList
+  };
+  defaultComponent = "Display Form";
+
+  render() {
+    const PatternName = this.components[
+      this.props.patternName || this.defaultComponent
+    ];
+    return (
+      <div className="pattern-container">
+        <PatternName showDescription={true} />
+      </div>
+    );
+  }
+}
+export default UIShellBody;
---
+++ client/src/pattern-components/SimpleList.jsx
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import {
+  StructuredListWrapper,
+  StructuredListRow,
+  StructuredListCell,
+  StructuredListHead,
+  StructuredListBody,
+  StructuredListInput,
+  Icon
+} from "carbon-components-react";
+import { iconCheckmarkSolid } from "carbon-icons";
+import Header from "./Header";
+import "./patterns.scss";
+
+class SimpleList extends Component {
+  columns = ['SecretName', 'SecretValue'];
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      selectedRow: 0
+    };
+  }
+
+  onRowClick = id => {
+    this.setState({ selectedRow: id });
+  };
+
+  renderRow = (row, id) => {
+    return (
+      <StructuredListRow key={id} onClick={() => this.onRowClick(id)}>
+        <div>
+          <StructuredListInput
+            id={`row-${id}`}
+            value="row-0"
+            title="row-0"
+            name="row-0"
+            //defaultChecked={this.state.selectedRow === id}
+            checked={this.state.selectedRow === id}
+          />
+          <StructuredListCell>
+            <Icon
+              className="bx--structured-list-svg"
+              icon={iconCheckmarkSolid}
+            />
+          </StructuredListCell>
+        </div>
+
+        {this.columns.map(col => {
+
+          return (
+            <StructuredListCell key={col} className="simple-list-row">
+              {row[col]}
+            </StructuredListCell>
+          );
+        })}
+      </StructuredListRow>
+    );
+  };
+  componentDidMount() {
+    const apiUrl = '/health';
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        console.log(data.status)
+        this.setState({ items: data });
+      });
+  }
+  render() {
+    console.log(this.state.items);
+    console.log("==================");
+    const data = this.state.items;
+    return (
+      <div className="bx--grid pattern-container">
+        <Header
+          title="Secrets"
+          subtitle="Displaying Secret From Key Protect with Continues Delivery ArgoCD"
+        />
+        <div className="bx--row">
+          <div className="bx--col-xs-12">
+            <StructuredListWrapper selection border>
+              <StructuredListHead>
+                <StructuredListRow head>
+                  <StructuredListCell head />
+                  {this.columns.map(key => {
+                    return (
+                      <StructuredListCell head key={key}>
+                        {key.charAt(0).toUpperCase() +
+                          key.slice(1).replace(/([A-Z])/g, " $1")}
+                      </StructuredListCell>
+                    );
+                  })}
+                </StructuredListRow>
+              </StructuredListHead>
+
+              <StructuredListBody>
+                {data.map((row, i) => {
+                  return this.renderRow(row, i);
+                })}
+              </StructuredListBody>
+            </StructuredListWrapper>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SimpleList;
